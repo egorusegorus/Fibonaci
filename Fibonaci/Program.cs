170df5d7cd693aef8eaa06c98cf1939e59af7cf2
egorusegorus <tomaszokraszewski@gmail.com>
2024-09-26 16:39:37 +0200

    Projektdateien hinzufügen.

---
+++ Fibonaci/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Fibonaci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Fibonacis spiralle: ");
+            fibonaci( 0,25);
+        }
+
+        public static void fibonaci( int EndRange)
+        {
+            double a=0,b=1;
+            
+            
+            for (int i = 0; i < EndRange; i++)
+            {
+                Console.WriteLine(a);
+                Console.WriteLine(b);
+                a = a + b;
+                b = a + b;
+            }
+        }
+        public static void fibonaci(int start, int EndRange)
+        {
+            double a = start, b = a + 1;
+
+
+            for (int i = 0; i < EndRange; i++)
+            {
+                Console.WriteLine(a);
+                Console.WriteLine(b);
+                a = a + b;
+                b = a + b;
+            }
+        }
+    }
+}
